9bf7b17b0e33d00fb2e07246cd878fb2aece967a
dawsbot <dawsonbotsford@gmail.com>
2024-05-04 16:26:29 -0700

    user datas

--- src/Message.ts
+++ src/Message.ts
@@ -2,12 +2,18 @@ import {
   Message,
   isCastAddMessage,
   isCastRemoveMessage,
+  isLinkAddMessage,
+  isLinkRemoveMessage,
   isReactionAddMessage,
+  isReactionRemoveMessage,
+  isUserDataAddMessage,
 } from '@farcaster/hub-nodejs';
 import { MessageState } from '@farcaster/shuttle';
 import { AppDb } from './db/db';
 import { Casts } from './db/models/Casts.model';
+import { Links } from './db/models/Links.model';
 import { Reactions } from './db/models/Reactions.model';
+import { UserDatas } from './db/models/UserDatas.model';
 import { log } from './log';
 
 /**
@@ -18,6 +24,10 @@ export class MessageWriter {
   public constructor(appDb: AppDb) {
     this.appDb = appDb;
   }
+  /**
+   * Parse messages and update postgresql
+   * https://docs.farcaster.xyz/reference/hubble/datatypes/messages#_1-4-message-type
+   */
   public async writeMessage(message: Message, state: MessageState) {
     if (isCastAddMessage(message) && state === 'created') {
       const casts = new Casts(this.appDb);
@@ -26,13 +36,26 @@ export class MessageWriter {
       const casts = new Casts(this.appDb);
       await casts.deleteOne(message);
     } else if (isReactionAddMessage(message)) {
+      //   log.info('Adding message: ' + message.data.reactionBody);
       const reactions = new Reactions(this.appDb);
       await reactions.insertOne(message);
+    } else if (isReactionRemoveMessage(message)) {
+      const reactions = new Reactions(this.appDb);
+      await reactions.deleteOne(message);
+    } else if (isLinkAddMessage(message)) {
+      const links = new Links(this.appDb);
+      await links.insertOne(message);
+    } else if (isLinkRemoveMessage(message)) {
+      const links = new Links(this.appDb);
+      await links.deleteOne(message);
+    } else if (isUserDataAddMessage(message)) {
+      const userDatas = new UserDatas(this.appDb);
+      await userDatas.insertOne(message);
     } else {
       console.dir(message.data);
       log.info(
         'Message handler not implemented for this message type: ' +
-          message.data,
+          message.data?.type,
       );
       //   throw new Error(
       //     "Message handler not implemented for this message type: " +
